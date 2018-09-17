44c153ccc8dded531146d3391a322398ad389b5f
noupsz <tashvigooroo27@gmail.com>
2018-09-17 17:53:58 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/canberra/mygooglestreetviewapp/MainActivity.java
@@ -0,0 +1,92 @@
+package au.edu.canberra.mygooglestreetviewapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+import com.google.android.gms.maps.StreetViewPanorama;
+
+
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
+
+    static final LatLng UC = new LatLng(-35.2366583, 149.0868123);
+    private static final float ZOOM_BY = -1.0f;
+    long duration = 5000;
+    float tilt = 0;
+    private static final int PAN_BY = 180;
+    static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+
+        // Animate over a duration of 5000 milliseconds.
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .zoom(panorama.getPanoramaCamera().zoom + ZOOM_BY)
+                .bearing(panorama.getPanoramaCamera().bearing - PAN_BY)
+                .tilt(tilt)
+                .build();
+
+        panorama.animateTo(camera, duration);
+        panorama.setPosition(UC);
+
+        panorama.setPosition(MELBOURNE);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
